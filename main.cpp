b08a9c06b3bf7aadb945649178bc37df2a3d53c5
Kuss <2360314753@qq.com>
2020-05-22 18:41:26 +0800

    :recycle: Refactor: main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
-#include "Fancy.hpp"
+#include "fancy.hpp"
 
 using namespace std;
+using fancy::Fancy;
+using fancy::Color;
+using fancy::Style;
 
 int main() {
-    cout << Fancy::cyan("Keyword") << "\n";
+    Fancy _fancy;
+
+    cout << (_fancy | Color::Magenta | Style::Inverse)("Hello")
+         << ", "
+         << (_fancy.reset() | Style::Bold)("World")
+         << (_fancy | Color::Green)("!")
+         << "\n";
 }
